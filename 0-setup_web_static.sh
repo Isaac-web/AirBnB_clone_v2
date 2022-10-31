3d8f270eaf1e55420dfc24787e236e4a73820821
Isaac Takiy <kanytakiy@gmail.com>
2022-10-31 22:43:50 +0000

    Quick fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,7 +4,7 @@
 mkdir -p /data/web_static/releases/
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
-echoh "Web server is working..." > /data/web_static/releases/test/index.html
+echo "Web server is working..." > /data/web_static/releases/test/index.html
 
 #create the link
 LINK=/data/web_static/current
@@ -15,9 +15,25 @@ fi
 
 ln -s /data/web_static/releases/test/ $LINK
 
-chown ubuntu -R /data && chgrp ubuntu -R /data
-sed -i ,root /var/www/school, root /data/web_static/current/, /etc/nginx/nginx/conf
+#chown ubuntu -R /data && chgrp ubuntu -R /data
+echo > -e 'events {
+}
 
-systemclt restart nginx
+http {
+        server {
+                listen 80;
+                root /var/www/alx/html;
+                index index.html;
+
+                location \ {
+
+                }
+        }
+
+}
+
+' >  /etc/nginx/nginx.conf
+
+systemctl restart nginx
 
 exit 0
