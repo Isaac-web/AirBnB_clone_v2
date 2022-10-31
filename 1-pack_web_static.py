2b193d32d0753a263ded4a282fe361339f341504
Isaac Takiy <kanytakiy@gmail.com>
2022-10-31 23:34:14 +0000

    Quick Fix

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,15 +8,5 @@ def do_pack():
     Zip all the file in the web static
     folder into a single file
     """
-
-    path = (
-            "versions",
-            "web_static_<year><month><day><hour><minute><second>.tgz"
-            )
-
-    try:
-        local("tar -cvzf versions/web_static_20170314233357.tgz web_static")
-        return path
-    except Exception as e:
-        pass
-    return None
+    path="versions/web_static_20170314233357.tgz"
+    local("tar -cvzf versions/web_static_20170314233357.tgz web_static")
