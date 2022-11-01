1a465c732bd676eab2fd6013d9f9cd04a98a8934
Isaac Takiy <kanytakiy@gmail.com>
2022-11-01 06:21:57 +0000

    Quick Fix

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -12,7 +12,7 @@ def do_pack():
     local("mkdir -p versions")
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
     file_path = "versions/web_static_{}.tgz".format(timestamp)
-    zipped = local("tar -cfvz {} web_static".format(file_path))
+    zipped = local("tar cfvz {} web_static".format(file_path))
 
     if zipped.succeeded:
         return file_path
