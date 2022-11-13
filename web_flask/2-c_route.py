2f05c8902b8ab0c0acaf298ce1046bf43edc6f86
Isaac Takiy <kanytakiy@gmail.com>
2022-11-13 08:39:17 +0000

    Change permission

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+A simple flask application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    """This route returns hello HBNB"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """This route return HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_text(text):
+    """Returns 'C' followed by the text provided"""
+    text = text = text.replace("_", " ")
+    return "C is {}".format(text)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
