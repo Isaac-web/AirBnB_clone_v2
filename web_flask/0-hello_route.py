5e321bc5da836ba59c99fe05d5633e700c1ccbec
Isaac Takiy <kanytakiy@gmail.com>
2022-11-13 11:05:14 +0000

    modify 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """
-A minimum flask app
+A simple flask application
 """
 from flask import Flask
 
-
 app = Flask(__name__)
+
+
 @app.route("/")
 def index():
-    """This route returns a Hello World page"""
+    """This route returns hello HBNB"""
     return "Hello HBNB!"
 
 
