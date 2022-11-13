30943aae3ab42ff8040236c310905344406e617e
Isaac Takiy <kanytakiy@gmail.com>
2022-11-13 09:05:49 +0000

    Add 3-python_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,38 @@
+from flask import Flask
+"""
+A simple flask application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    """This route returns hello HBNB"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """This route return HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_text(text):
+    """Returns 'C' followed by the text provided"""
+    text = text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route("/python")
+@app.route("/python/")
+@app.route("/python/<text>")
+def python_is_cool(text="is cool"):
+    """Returns 'Python' followed by the text provided"""
+    return "Python {}".format(text.replace("_", " "))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
