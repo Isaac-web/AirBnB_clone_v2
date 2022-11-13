94c75ead29ed975345dcb6a41f1ed7b3035c405f
Isaac Takiy <kanytakiy@gmail.com>
2022-11-13 07:56:25 +0000

    Change port

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,8 +9,8 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     """This route returns a Hello World page"""
-    return "Hello HBNB"
+    return "Hello HBNB!"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=5000)
