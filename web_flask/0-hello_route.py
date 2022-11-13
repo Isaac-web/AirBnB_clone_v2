f0018fc84216448fe5d606cd6d308aaec0bb6d77
Isaac Takiy <kanytakiy@gmail.com>
2022-11-13 07:50:51 +0000

    Add documentation

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+"""
+A minimum flask app
+"""
 from flask import Flask
 
 
 app = Flask(__name__)
 @app.route("/")
-def hello():
+def index():
+    """This route returns a Hello World page"""
     return "Hello HBNB"
 
 
