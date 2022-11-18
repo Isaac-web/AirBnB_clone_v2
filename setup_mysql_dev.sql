9cfc5745050373f0a6a8e33c6343a2af594572c4
Isaac Takiy <kanytakiy@gmail.com>
2022-11-18 16:54:42 +0000

    Quick fix

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,6 @@
--- Prepares the database for the project
+-- Prepares the database for the projec
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev_pwd'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev@''localhost';
-FLUSH PRIVILEGES;
+
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
